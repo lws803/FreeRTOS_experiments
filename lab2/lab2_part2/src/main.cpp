8c3cc07b095f8d7f1f500e062af06e5c180107f7
lws803 <lws803@gmail.com>
2018-09-10 09:36:15 +0800

    Added part 2 again

--- lab2/lab2_part2/src/main.cpp
+++ lab2/lab2_part2/src/main.cpp
@@ -4,14 +4,12 @@
 unsigned long interrupt_time_1 = millis();
 unsigned long interrupt_time_2 = millis();
 
-TPrioQueue *queue;
-
+TPrioQueue *queue; // Make new queue
 // Declares a new type called "funcptr"
-
 typedef void (*funcptr)(void);
-
 // Flashes LED at pin 6: 5 times a 5 Hz
 void int0task() {
+    // Loop to make it blink 5 times 
     for (int i = 0; i < 5; i++) {
         digitalWrite(6, HIGH);
         delay(100);
@@ -21,6 +19,7 @@ void int0task() {
 }
 // Flashes LED at pin 7: 5 times at 1 Hz
 void int1task() {
+    // Loop to make it blink 5 times 
     for (int i = 0; i < 5; i++) {
         digitalWrite(7, HIGH);
         delay(500);
@@ -28,7 +27,6 @@ void int1task() {
         delay(500);
     }
 }
-
 void int0ISR() {
     unsigned long curr_time = millis();
     // Curr time is taken to measure against the previous time when the interrupt was called, 
@@ -38,9 +36,7 @@ void int0ISR() {
         Serial.println("enq 0"); // Printing for debugging
         enq(queue, (void *)int0task, 9);
     }
-
 }
-
 void int1ISR() {
     unsigned long curr_time = millis();
     // Curr time is taken to measure against the previous time when the interrupt was called, 
@@ -49,17 +45,14 @@ void int1ISR() {
         interrupt_time_2 = curr_time;
         Serial.println("enq 1"); // Printing for debugging
         enq(queue, (void *)int1task, 0);
-
     }
 }
-
 void setup() {
     queue=makeQueue();
     Serial.begin(9600);
     attachInterrupt(digitalPinToInterrupt(2), int0ISR, RISING);
     attachInterrupt(digitalPinToInterrupt(3), int1ISR, RISING);
 }
-
 // Dequeues and calls functions if the queue is not empty
 void loop() {
     if(qlen(queue)>0) {
@@ -69,5 +62,4 @@ void loop() {
         function();
         Serial.println("deq-ed");
 	}	
-
 }
