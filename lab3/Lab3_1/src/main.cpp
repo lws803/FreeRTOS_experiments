ad888f06e735a1f51b93e66a42639445d3e1c459
Wilson Ler <lws803@gmail.com>
2018-10-01 23:19:34 +0700

    Made necesarry changes to lab 3

--- lab3/Lab3_1/src/main.cpp
+++ lab3/Lab3_1/src/main.cpp
@@ -4,26 +4,23 @@
 #include <task.h>
 
 #define STACK_SIZE 200 
-#define LED_PIN 6
+#define LED_PIN 11
 
 void task1(void *p) {
     for (;;) { 
-        digitalWrite(6, HIGH); 
-        delay(1000); 
-        digitalWrite(6, LOW); 
-        delay(1000);
+            digitalWrite(LED_PIN, HIGH); 
+            delay(1000); 
+            digitalWrite(LED_PIN, LOW); 
+            delay(1000);
+
         } 
     }
 void setup() {
-       pinMode(LED_PIN, OUTPUT);
-}
-void loop() { 
+    pinMode(LED_PIN, OUTPUT);
+    
     xTaskCreate( task1, "Task1", STACK_SIZE, NULL, 1, NULL);
-// created task can be referenced.
-// Pointer to the task entry function
-// Task name
-// Stack size
-// Pointer that will be used as parameter // Task priority
-// Used to pass back a handle by which the
     vTaskStartScheduler();
+}
+
+void loop() { 
 }
